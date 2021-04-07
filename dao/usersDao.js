f461c62dc3b46e2336cdfe56d7b6a8c006d43175
yuanqingyan <17852613880@163.com>
2021-04-07 20:21:53 +0800

    登录接口增加md5加密密码

--- dao/usersDao.js
+++ dao/usersDao.js
@@ -1,14 +1,16 @@
 var databaseModules = require('../modules/database');
+var md5 = require('md5');
 
 module.exports.login = (loginStr, pwd, cb) => {
   const conn = databaseModules.getConnection();
   databaseModules.connect(conn);
-  const sql = `select username, account, enterprise_code as code from admin where username = ${loginStr} and password = ${pwd} or account = ${loginStr} and password = ${pwd}`;
+  const sql = `select username, account, enterprise_code as code from admin where username = '${loginStr}' and password = '${md5(pwd)}' or account = '${loginStr}' and password = '${md5(pwd)}'`;
   conn.query(sql, (err, result) => {
     if (err) {
       cb(err);
+    } else if (result) {
+      cb(null, result);
     }
-    cb(null, result);
   })
   conn.end();
 }
